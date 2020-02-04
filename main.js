658ed4a7a5ae4073034d9c70a4194962d689a1cb
unknown <christopher.carleson@gmail.com>
2020-02-04 11:10:44 +0100

    Working on productArray

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // Börjar med skapandet av varje produkts ruta - Christopher
-// let productSection = document.querySelector('.posterMenu');
-// console.log(productSection);
-// let product = []
-// let productHeadline = [];
-// let productImage = []   //Need to add url for all the images
+let productSection = document.querySelector('.posterMenu');
+
+let productCounter = 0; // Needed to find index of products in forEach on row 20.
+
 
 fetch('main.json')
     .then(resp => resp.json())
@@ -14,23 +13,16 @@ fetch('main.json')
 
 function productContent(products){
     console.log(products.products[0]);
-    let testar = JSON.stringify(products);
-    console.log(testar);
-    
-    for(let i=0; i>12; i++){
-        console.log(products.products[3]);
+    let productArray = products.products;
+    // let testar = JSON.stringify(products.products);
+    console.log(productArray[0]);
+
+    productArray.forEach(element => {
+        console.log(element);
         
-        // product[i] = document.createElement('artile');
-        // productSection.appendChild(product[i]);
-        // console.log(productSection);
-        // productHeadline[i] = document.createElement('h3');
-        // product[i].appendChild(productHeadline[i]);
-        // console.log(product[i]);
-    
-        
-        
-    };
+    });
 }
+
 
 
 
