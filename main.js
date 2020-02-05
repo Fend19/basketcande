4f93bc0a771ec26a2c16f39c8a37b53e8f95bf44
unknown <christopher.carleson@gmail.com>
2020-02-05 14:56:31 +0100

    Added amount for productselection and category in json-file

--- main.js
+++ main.js
@@ -12,16 +12,18 @@ fetch('main.json')
 
 
 function productContent(products){
-    console.log(products.products[0]);
+    // console.log(products.products[0]);
     let productArray = products.products;
     // let testar = JSON.stringify(products.products);
-    console.log(productArray[0]);
+    // console.log(productArray[0]);
 
     productArray.forEach(element => {
         // Creating container for each product
         let productSection = document.createElement('section');
         productSection.className = 'product-' + productCounter;
+        // productSection.classList.add('productSection');
         document.querySelector('.posterArticle').appendChild(productSection);
+
             // *****Creating elements and placing them in productcontainer(aka productSection)*****
         // Creating product-headline
         let productHeadline = document.createElement('h3');
@@ -41,10 +43,26 @@ function productContent(products){
         productPrice.innerText = 'Price: ' + element.price;
         document.querySelector("[class=product-" + productCounter + "]").appendChild(productPrice);
         console.log(element.price);
+        // Creating input for amount of specified product the should be added to the basket
+        let productAmount = document.createElement('input');
+        productAmount.type = 'number';
+        productAmount.min = 1;
+        productAmount.placeholder = 'Amount';
+        productAmount.className = productCounter + '-input';
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productAmount);
+
         // Creating buy-button for product
         let productButtonBuy = document.createElement('button');
         productButtonBuy.innerText = 'Add to Cart';
         document.querySelector("[class=product-" + productCounter + "]").appendChild(productButtonBuy);
+
+        // Adding eventlistener to the buybutton to get value of quantity selected
+        let amountSelected = 0;
+        productButtonBuy.addEventListener('click', function(){
+            amountSelected += parseInt(productAmount.value);
+            console.log(amountSelected);
+            
+        })
 
         console.log(productCounter);
         productCounter++; // Variable for the index of object in productArray
