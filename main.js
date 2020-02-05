7c9ec10e5fd5c153bc75be224d07275db7c94505
Elin Bostrom <ebostrom@live.se>
2020-02-05 20:07:32 +0100

    added sections for product text and image and styled the products

--- main.js
+++ main.js
@@ -22,29 +22,45 @@ function productContent(products) {
         productSection.className = 'product-' + productCounter;
         document.querySelector('.posterArticle').appendChild(productSection);
         // *****Creating elements and placing them in productcontainer(aka productSection)*****
-        // Creating product-headline
-        let productHeadline = document.createElement('h3');
-        productHeadline.innerText = element.title;
-        document.querySelector("[class=product-" + productCounter + "]").appendChild(productHeadline);
-        console.log(element.title);
+
+        // Creating a section for productTextInfo and for productImgWrapper
+        let productTextInfo = document.createElement('section');
+        productTextInfo.className = 'productTextInfo'
+        let productImgWrapper = document.createElement('div');
+
         // Creating product-image
         let productImage = document.createElement('img');
         productImage.setAttribute('src', element.image);
         productImage.className = 'imgWrapper';
-        document.querySelector("[class=product-" + productCounter + "]").appendChild(productImage);
+        productImgWrapper.appendChild(productImage);
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productImgWrapper);
+
+        // Creating product-headline
+        let productHeadline = document.createElement('h3');
+        productHeadline.innerText = element.title;
+        productTextInfo.appendChild(productHeadline)
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productTextInfo);
+        console.log(element.title);
+
         // Creating product "information"
         let productLorem = document.createElement('p');
-        productLorem.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
-        document.querySelector("[class=product-" + productCounter + "]").appendChild(productLorem);
+        productLorem.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        productTextInfo.appendChild(productLorem)
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productTextInfo);
+
         // Creating product-pricetag
         let productPrice = document.createElement('h4');
-        productPrice.innerText = 'Price: ' + element.price;
-        document.querySelector("[class=product-" + productCounter + "]").appendChild(productPrice);
+        productPrice.innerText = 'Price: ' + element.price + ':-';
+        productTextInfo.appendChild(productPrice)
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productTextInfo);
         console.log(element.price);
+
         // Creating buy-button for product
         let productButtonBuy = document.createElement('button');
         productButtonBuy.innerText = 'Add to Cart';
-        document.querySelector("[class=product-" + productCounter + "]").appendChild(productButtonBuy);
+        productButtonBuy.className = 'buyButtonStyler';
+        productTextInfo.appendChild(productButtonBuy)
+        document.querySelector("[class=product-" + productCounter + "]").appendChild(productTextInfo);
 
         console.log(productCounter);
         productCounter++; // Variable for the index of object in productArray
