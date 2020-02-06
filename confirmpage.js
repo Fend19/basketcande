32730dd29ee43ffe87b5b39eb4d3d5e129848291
Elin Bostrom <ebostrom@live.se>
2020-02-06 15:57:52 +0100

    added different js files

---
+++ confirmpage.js
@@ -0,0 +1,7 @@
+  // Append elements to this declaration
+  let confirmpageSection = (document.querySelector(
+    ".confirmArticle"
+  ).innerHTML = "hej");
+  let confirmpage = document.querySelector(".mainConfirmPage");
+  confirmpage.appendChild(confirmpageSection);
+  console.log(confirmpageSection);
